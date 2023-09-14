d1bee3d166b656578882d38f039a71765e39cf29
Raymond-Moody <rem26@njit.edu>
2023-09-13 21:59:04 -0400

    Implemented basic REST call

--- src/App.js
+++ src/App.js
@@ -1,11 +1,30 @@
-//import './App.css';
+import axios from 'axios';
+import React from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <h2>Test</h2>
-    </div>
-  );
+class App extends React.Component{
+
+    constructor(props){
+        super(props);
+        this.state = {status : ""};
+    }
+
+    healthCheck = () => {
+        axios
+            .get("http://localhost:8000/api/rest")
+            .then(response => this.setState({status : response.data.message}))
+            .catch(err => console.log(err));
+    }
+
+    render(){
+        return (
+            <div className="App">
+                <h2>Health Check</h2>
+                <div id="status">{this.state.status}</div>
+                <button onClick={this.healthCheck}>Test Backend</button>
+            </div>
+        );
+    }
 }
+
 
 export default App;
