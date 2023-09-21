ed8e7eb3c6fe798c2fe019212dd1b856ade1bfe0
Raymond-Moody <rem26@njit.edu>
2023-09-20 23:43:50 -0400

    Added Top 5 Movie list

--- src/App.js
+++ src/App.js
@@ -1,30 +1,87 @@
 import axios from 'axios';
 import React from 'react';
+import { useState } from 'react';
+import { useEffect } from 'react';
 
-class App extends React.Component{
+export default function App(){
 
-    constructor(props){
-        super(props);
-        this.state = {status : ""};
-    }
-
-    healthCheck = () => {
+/* Old milestone 1 code
+    const [status, setStatus] = useState('');
+    function healthCheck() {
         axios
-            .get("http://localhost:8000/api/rest")
-            .then(response => this.setState({status : response.data.message}))
+            .get("http://localhost:8000/rest")
+            .then(response => setStatus(response.data.message))
             .catch(err => console.log(err));
     }
 
-    render(){
-        return (
+    return(
             <div className="App">
                 <h2>Health Check</h2>
-                <div id="status">{this.state.status}</div>
-                <button onClick={this.healthCheck}>Test Backend</button>
+                <div id="status">{status}</div>
+                <button onClick={healthCheck}>Test Backend</button>
             </div>
-        );
+    )
+*/
+    const [filmList, setFilmList] = useState([]);
+    const [selectedFilm, setSelectedFilm] = useState(0);
+    const [renderData, setRenderData] = useState(false);
+    
+    function getTopFilms(){
+        axios
+            .get("http://localhost:8000/films/top-movies")
+            .then(response => setFilmList(response.data))
+            .catch(err => console.log(err));
     }
+
+    function handleClick(id){
+        if(selectedFilm === id){
+            setRenderData(!renderData);
+        } else {
+            setSelectedFilm(id);
+            setRenderData(true);
+        }
+    }
+
+    useEffect(() => {
+        getTopFilms();
+    }, []);
+
+    return(
+        <div className="App">
+            <h1>Top 5 Movies</h1>
+            <ol>
+                {
+                    filmList.map(
+                        film => <li key={film['film_id']} onClick={() => handleClick(film['film_id'])}>{film['title']}</li>
+                    )
+                }
+            </ol>
+            {renderData && <FilmInfo id={selectedFilm}/>}
+        </div>
+    );
 }
 
+function FilmInfo({ id }){
+    const [filmData, setFilmData] = useState({});
 
-export default App;
+    function getFilmData(id){
+        axios
+            .get(`http://localhost:8000/films/${id}`)
+            .then(response => setFilmData(response.data))
+            .catch(err => console.log(err));
+    }
+
+    return (
+        <div className="FilmInfo">
+            {getFilmData(id)}
+            <table>
+                <tr><td>Title: </td><td>{filmData['title']}</td></tr>
+                <tr><td>Description: </td><td>{filmData['description']}</td></tr>
+                <tr><td>Release Year: </td><td>{filmData['release_year']}</td></tr>
+                <tr><td>Length: </td><td>{filmData['length']} minutes</td></tr>
+                <tr><td>Rating: </td><td>{filmData['rating']}</td></tr>
+                <tr><td>Special Features: </td><td>{filmData['special_features']}</td></tr>
+            </table>
+        </div>
+    );
+}
